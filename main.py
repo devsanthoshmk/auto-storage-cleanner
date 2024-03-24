09da1a2207c0696cf296270868fc5bac5f231bf1
devsanthoshmk <kavimanisan@gmail.com>
2024-03-24 14:35:38 +0530

    checking dir con

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import datetime
 import os
 
 
-image_path = "/home/santhosh/Pictures/pallikaranai nadarajan.png"
+image_path = os.listdir("/storage/emulated/0/")
 # image_path = "/storage/emulated/0/Acode/delcheck.jpg"
-creation_time = os.path.getctime(image_path)
-three_months_ago = datetime.datetime.now() - datetime.timedelta(days=3*30)
-if creation_time < three_months_ago.timestamp():
-    print("yes")
-    os.remove(image_path)
+for i in image_path:
+    creation_time = os.path.getctime(i)
+    three_months_ago = datetime.datetime.now() - datetime.timedelta(days=3*30)
+    print(creation_time)
+    if creation_time > three_months_ago.timestamp():
+        print("yes three months older")
+        # os.remove(image_path)
